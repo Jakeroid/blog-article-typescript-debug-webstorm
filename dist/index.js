993edf21f36e1b3be9cd2f9a17dcdf7959199101
Ivan Karabadzhak <jakeroid@icloud.com>
2023-02-21 11:23:55 +0200

    Initial commit.

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function main() {
+    console.log('Hello World! We will setup TypeScript debugging in WebStorm!');
+    for (let i = 0; i < 10; i++) {
+        console.log(`Debugging is fun! ${i}`);
+    }
+}
+main();
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+function main() {
+
+    console.log('Hello World! We will setup TypeScript debugging in WebStorm!');
+    for (let i = 0; i < 10; i++) {
+        console.log(`Debugging is fun! ${i}`);
+    }
+}
+
+main();
